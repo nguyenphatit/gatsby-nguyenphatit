4b7967f4091f395efc596a97f212f2f5e4af42da
Phat <nguyenphatit.nl@gmail.com>
2023-03-20 23:28:47 +0700

    Update env

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -28,7 +28,7 @@ const config: GatsbyConfig = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          process.env.GA, // Google Analytics / GA
+          process.env.GATSBY_GA, // Google Analytics / GA
           // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
           // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
