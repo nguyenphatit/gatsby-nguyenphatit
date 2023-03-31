ff349fa4dfbc1dfa2472de3a5358c7fd57cde7bc
Phat <nguyenphatit.nl@gmail.com>
2023-03-31 17:39:18 +0700

    Update cursor and menu

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroComponent: React.FC<{}> = () => {
   return (
-    <div className="min-h-screen bg-[url('../images/svg/hero-background.svg')] bg-center bg-no-repeat bg-cover flex justify-center items-center">
+    <div className="min-h-screen bg-black bg-center bg-no-repeat bg-cover flex justify-center items-center">
       <div className="container mx-auto flex justify-center items-center flex-col">
         <h1 className="text-9xl font-serif uppercase text-green-dark">Hello</h1>
         <h1 className="text-9xl font-serif uppercase text-green-dark">Hello</h1>
